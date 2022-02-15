29ce009d4764c0a7fd0427665e6321fd8a56bcdf
Selin Karademir <47951360+selinkarademir@users.noreply.github.com>
2022-02-15 17:36:39 +0300

    Create initial version of animals.py

---
+++ animal.py
@@ -0,0 +1,11 @@
+#this is the remote repo. the server
+import sys
+
+class Animal(object):
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+    
+    def speak(self):
+      print("i am")
+    
